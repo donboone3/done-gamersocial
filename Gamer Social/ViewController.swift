a79be3a728d77b8232f18a05e078a25f025557f9
Peyton <donboone3@dons-mac.local>
2017-01-03 16:51:09 -0800

    Initial Commit

---
+++ Gamer Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gamer Social
+//
+//  Created by Don Boone on 1/3/17.
+//  Copyright © 2017 Don Boone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
